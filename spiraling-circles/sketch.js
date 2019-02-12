856ddbfbd0090fd7e9d7648c90145dcc98fcb1b0
Alexander Whitaker <whitaker.alexander@gmail.com>
2019-02-11 21:08:08 -0600

    add multiple circles

--- spiraling-circles/sketch.js
+++ spiraling-circles/sketch.js
@@ -1,5 +1,3 @@
-
-
 class Particle {
     constructor(x, y, radius, color) {
         this.initialX = x
@@ -8,35 +6,48 @@ class Particle {
         this.y = y
         this.radius = radius
         this.color = color
-        this.radians = 0
+        this.radians = Math.random() * Math.PI * 2
+        this.distanceFromCenter = random(50, 120)
         this.velocity = 0.05
     }
 
     update() {
+        const oldLocation = { x: this.x, y: this.y }
         this.radians += this.velocity
-        this.x = this.initialX + Math.cos(this.radians) * 100
-        this.y = this.initialY + Math.sin(this.radians) * 100
-        this.draw()
+        this.x = this.initialX + Math.cos(this.radians) * this.distanceFromCenter
+        this.y = this.initialY + Math.sin(this.radians) * this.distanceFromCenter
+        this.draw(oldLocation)
     }
 
-    draw() {
-        noStroke()
-        fill(this.color)
-        ellipse(this.x, this.y, this.radius, this.radius)
+    draw(oldLocation) {
+        // Draw Circles
+        // noStroke()
+        // fill(this.color)
+        // ellipse(this.x, this.y, this.radius, this.radius)
+
+        // Draw line
+        strokeWeight(this.radius)
+        stroke(this.color)
+        line(oldLocation.x, oldLocation.y, this.x, this.y)
     }
 }
 
 let particles = []
+
 function setup() {
     createCanvas(400, 400)
+    const colors = ['steelblue', 'black', 'darkblue']
 
-    for (let i = 0; i < 1; i++) {
-        particles.push(new Particle(height / 2, width / 2, 10, 'steelblue'))
+    for (let i = 0; i < 50; i++) {
+        const color = colors[[Math.floor(random(0, 2))]]
+        const radius = random(1, 2)
+        particles.push(new Particle(height / 2, width / 2, radius, color))
     }
 }
 
 function draw() {
     background('rgba(255, 255, 255, 0.05)')
+    // background('white')
     particles.forEach(particle => particle.update())
 }
 

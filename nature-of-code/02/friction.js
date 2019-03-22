d465bea80b17e080163796bef30395edd2639d7b
Alexander Whitaker <whitaker.alexander@gmail.com>
2019-03-21 20:32:39 -0500

    friction

--- nature-of-code/02/friction.js
+++ nature-of-code/02/friction.js
@@ -6,10 +6,10 @@ class Mover {
     mass;
 
     constructor() {
-        this.position = createVector(random(0, width), random(0, height));
+        this.position = createVector(20, height / 2);
         this.velocity = createVector(0, 0);
         this.acceleration = createVector(0, 0);
-        this.mass = random(0.1, 2);
+        this.mass = 1
     }
 
     applyForce(force) {
@@ -21,6 +21,7 @@ class Mover {
         this.velocity.add(this.acceleration);
         this.position.add(this.velocity);
         this.acceleration.mult(0);
+        this.velocity.limit(5)
     }
 
     draw() {
@@ -32,16 +33,12 @@ class Mover {
 
     checkEdges() {
         if (this.position.x > width) {
-            this.position.x = width;
-            this.velocity.x *= -1;
+            this.position.x = 0;
+
+            // this.velocity.x *= -1;
         } else if (this.position.x < 0) {
             this.velocity.x *= -1;
             this.position.x = 0;
-        }
-
-        if (this.position.y > height) {
-            this.velocity.y *= -1;
-            this.position.y = height;
         }
     }
 
@@ -52,31 +49,28 @@ let movers = []
 function setup() {
     createCanvas(600, 400)
 
-    for(let i = 0; i < 1; i++) {
+    for (let i = 0; i < 1; i++) {
         movers.push(new Mover())
     }
 }
 
 function draw() {
     background('white')
-    const gravity = createVector(0, 0.2)
-    const wind = createVector(random(0, 1), 0)
-    const c = -0.01;
-
+    stroke('black')
+    line(0, height / 2 + 15, width, height / 2 + 15)
     movers.forEach((mover) => {
-
-        gravity.mult(mover.mass)
-        mover.applyForce(gravity)
-
+        
         const friction = mover.velocity.copy();
         friction.normalize()
-        friction.mult(-1)
-        friction.mult(c)
 
+        if (keyIsPressed) {
+            friction.mult(-0.1)
+        } else {
+            friction.mult(0.1)
+        }
         mover.applyForce(friction)
 
-        // mover.applyForce(wind)
-
+        mover.applyForce(createVector(0.05, 0))
 
         mover.update()
         mover.checkEdges()
